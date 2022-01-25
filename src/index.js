0610d94242796150a6f6593f00e14fe39122c18b
Mike543 <p890098@gmail.com>
2022-01-25 18:06:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const arr = [];
+  for(let i = 0 ; i < bracketsConfig.length ; i++){
+    arr.push(bracketsConfig[i][0]+bracketsConfig[i][1]);
+  }
+  for (let j = 0; j < str.length; j++) {
+    if(str.includes(arr[j])) {
+      str = str.replace(arr[j], "");
+      j = -1;
+    }
+  }
+  if(str.length === 0){
+    return true;
+  } else {
+    return false;
+  }
+};
